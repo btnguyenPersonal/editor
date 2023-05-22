2230988e13aeac9dd0baa4e504f17c474af46f32
btnguyenPersonal <btnguyenPersonal@gmail.com>
2023-05-22 16:10:21 -0500

    fixed cursor movement

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use crossterm::event::{Event, KeyCode, KeyEvent};
-use crossterm::cursor::{MoveLeft, MoveRight, MoveUp, MoveDown};
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
 use crossterm::execute;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
 use crossterm::terminal::{Clear, ClearType};
-use crossterm::cursor::MoveToColumn;
-use crossterm::style::{Print};
+use crossterm::cursor::{MoveToColumn, MoveToRow};
+use crossterm::style::{Color, Print, SetForegroundColor};
 use std::fs::File;
 use std::io::{self, stdout, BufRead, BufReader};
+use crossterm::terminal::size;
+
 
 fn get_file_data(file_name: &str) -> io::Result<Vec<String>> {
     let file_path = format!("{}", file_name);
@@ -20,12 +21,32 @@ fn render_file_data(file_data: &[String], cursor_x: usize, cursor_y: usize) {
     let mut stdout = stdout();
 
     execute!(stdout, Clear(ClearType::All)).expect("Failed to clear screen");
+    let terminal_size = size().unwrap();
+    let term_height = terminal_size.1 as usize;
+    let mut y = 0;
 
-    for line in file_data {
-        execute!(stdout, MoveToColumn(0), Print(line), Print("\n")).expect("Failed to execute command");
+    execute!(stdout, MoveToRow(0)).expect("Failed to move cursor");
+    while y <= term_height && y < file_data.len() {
+        let line: &String = &file_data[y];
+        execute!(
+            stdout,
+            MoveToColumn(0),
+            SetForegroundColor(Color::DarkGrey),
+            Print(format!("{:5} ", y + 1)),
+            SetForegroundColor(Color::White),
+            Print(line),
+            Print("\n")
+        ).expect("Failed to execute command");
+        y += 1;
     }
-    execute!(stdout, MoveUp(file_data.len() as u16 - cursor_y as u16)).expect("Failed to move cursor");
-    execute!(stdout, MoveToColumn(cursor_x as u16)).expect("Failed to move cursor");
+    execute!(stdout, MoveToRow(cursor_y as u16 + 1)).expect("Failed to move cursor");
+    let cursor_x_display: u16 = if cursor_x > file_data[cursor_y].len() {
+        file_data[cursor_y].len().try_into().unwrap()
+    } else {
+        cursor_x.try_into().unwrap()
+    };
+    execute!(stdout, MoveToColumn(cursor_x_display as u16 + 7)).expect("Failed to move cursor");
+
 }
 
 fn quit_terminal() {
@@ -35,23 +56,14 @@ fn quit_terminal() {
 }
 
 fn main() {
-    // Get the command-line arguments
     let args: Vec<String> = std::env::args().collect();
-
-    // Check if a file name argument was provided
     if args.len() < 2 {
         println!("Please provide a file name");
         return;
     }
-
-    // Get the file name from the arguments
     let file_name = &args[1];
-
-    // Enable raw mode and enter alternate screen
     enable_raw_mode().expect("Failed to enable raw mode");
     execute!(stdout(), EnterAlternateScreen).expect("Failed to enter alternate screen");
-
-    // Render file data
     let file_data = match get_file_data(file_name) {
         Ok(data) => data,
         Err(err) => {
@@ -63,41 +75,36 @@ fn main() {
     let mut cursor_x = 0;
     let mut cursor_y = 0;
     render_file_data(&file_data, cursor_x, cursor_y);
-
-    // Wait for Ctrl+C to exit
     loop {
         if let Ok(event) = crossterm::event::read() {
-            if let Event::Key(KeyEvent { code, .. }) = event {
-                match code {
-                    KeyCode::Char('c') => break,
-                    KeyCode::Char('h') => {
-                        if cursor_x > 0 {
-                            cursor_x -= 1;
-                        }
+            if let Event::Key(KeyEvent { code, modifiers }) = event {
+                if code == KeyCode::Char('c') && modifiers.contains(KeyModifiers::CONTROL) {
+                    break;
+                } else if code == KeyCode::Char('h') {
+                    if cursor_x > file_data[cursor_y].len() {
+                        cursor_x = file_data[cursor_y].len() - 1;
+                    } else if cursor_x > 0 {
+                        cursor_x -= 1;
                     }
-                    KeyCode::Char('l') => {
-                        if cursor_x < file_data[cursor_y].len() {
-                            cursor_x += 1;
-                        }
+                    render_file_data(&file_data, cursor_x, cursor_y);
+                } else if code == KeyCode::Char('l') {
+                    if cursor_x < file_data[cursor_y].len() {
+                        cursor_x += 1;
                     }
-                    KeyCode::Char('k') => {
-                        if cursor_y > 0 {
-                            cursor_y -= 1;
-                        }
+                    render_file_data(&file_data, cursor_x, cursor_y);
+                } else if code == KeyCode::Char('j') {
+                    if cursor_y < file_data.len() - 1 {
+                        cursor_y += 1;
                     }
-                    KeyCode::Char('j') => {
-                        if cursor_y < file_data.len() - 1 {
-                            cursor_y += 1;
-                        }
+                    render_file_data(&file_data, cursor_x, cursor_y);
+                } else if code == KeyCode::Char('k') {
+                    if cursor_y > 0 {
+                        cursor_y -= 1;
                     }
-                    _ => {}
+                    render_file_data(&file_data, cursor_x, cursor_y);
                 }
-
-                render_file_data(&file_data, cursor_x, cursor_y);
             }
         }
     }
-
-    // Quit the terminal
     quit_terminal();
 }
