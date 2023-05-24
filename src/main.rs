4f7f6b11b088460aad0f13e25f0141dc0dd4c426
btnguyenPersonal <btnguyenPersonal@gmail.com>
2023-05-23 20:25:54 -0500

    got window_lines working

--- src/main.rs
+++ src/main.rs
@@ -16,35 +16,39 @@ fn get_file_data(file_name: &str) -> io::Result<Vec<String>> {
     Ok(lines)
 }
 
-fn render_file_data(file_data: &[String], cursor_x: usize, cursor_y: usize) {
+fn render_file_data(file_data: &[String], window_line_x: usize, window_line_y: usize, cursor_x: usize, cursor_y: usize) {
     let mut stdout = stdout();
 
     execute!(stdout, Clear(ClearType::All)).expect("Failed to clear screen");
     let terminal_size = size().unwrap();
     let term_height = terminal_size.1 as usize;
+    let term_width = terminal_size.0 as usize;
     let mut y = 0;
 
-    execute!(stdout, MoveToRow(0)).expect("Failed to move cursor");
-    while y <= term_height && y < file_data.len() {
-        let line: &String = &file_data[y];
+    while y < term_height && y < file_data.len() {
+        execute!(stdout, MoveToRow(((y + 1) as u16).try_into().unwrap())).expect("Failed to move cursor");
+        let mut line: String = if file_data[window_line_y + y].len() >= window_line_x { file_data[window_line_y + y][window_line_x..].to_string() } else { "".to_string() };
+        if line.len() > term_width {
+            let substring = &line[..term_width];
+            line = String::from(substring);
+        }
         execute!(
             stdout,
-            MoveToColumn(0),
+            MoveToColumn(1),
             SetForegroundColor(Color::DarkGrey),
-            Print(format!("{:5} ", y + 1)),
+            Print(format!("{:4} ", window_line_y + y + 1)),
             SetForegroundColor(Color::White),
-            Print(line),
-            Print("\n")
+            Print(line)
         ).expect("Failed to execute command");
         y += 1;
     }
-    execute!(stdout, MoveToRow(cursor_y as u16 + 1)).expect("Failed to move cursor");
+    execute!(stdout, MoveToRow(cursor_y as u16 + 1 - window_line_y as u16)).expect("Failed to move cursor");
     let cursor_x_display: u16 = if cursor_x > file_data[cursor_y].len() {
         file_data[cursor_y].len().try_into().unwrap()
     } else {
-        cursor_x.try_into().unwrap()
+        cursor_x as u16 - window_line_x as u16
     };
-    execute!(stdout, MoveToColumn(cursor_x_display as u16 + 7)).expect("Failed to move cursor");
+    execute!(stdout, MoveToColumn(cursor_x_display as u16 + 6)).expect("Failed to move cursor");
 
 }
 
@@ -98,6 +102,28 @@ fn reset_cursor_end(file_data: &[String], cursor_x: usize, cursor_y: usize) -> u
     }
 }
 
+fn calc_window_lines(file_data: &[String], window_line_x: usize, window_line_y: usize, cursor_x: usize, cursor_y: usize) -> (usize, usize) {
+    let terminal_size = size().unwrap();
+    let term_height = terminal_size.1 as usize;
+    let term_width = terminal_size.0 as usize;
+    let mut x = window_line_x;
+    let mut y = window_line_y;
+    let cursor_display_x = if cursor_x > file_data[cursor_y].len() { file_data[cursor_y].len() } else { cursor_x };
+    if window_line_x + (term_width - 6) <= cursor_display_x {
+        x = cursor_display_x - (term_width - 6);
+    }
+    if window_line_x > cursor_display_x {
+        x = cursor_display_x;
+    }
+    if window_line_y > cursor_y {
+        y = cursor_y;
+    }
+    if window_line_y + term_height <= cursor_y {
+        y = cursor_y - term_height + 1;
+    }
+    (x, y)
+}
+
 fn count_leading_spaces(input: &str) -> usize {
     let trimmed = input.trim_start();
     let leading_spaces = input.len() - trimmed.len();
@@ -121,10 +147,12 @@ fn main() {
             return;
         }
     };
+    let mut window_line_x = 0;
+    let mut window_line_y = 0;
     let mut cursor_x = 0;
     let mut cursor_y = 0;
     let mut mode = 'n';
-    render_file_data(&file_data, cursor_x, cursor_y);
+    render_file_data(&file_data, window_line_x, window_line_y, cursor_x, cursor_y);
     loop {
         if let Ok(event) = crossterm::event::read() {
             if let Event::Key(KeyEvent { code, modifiers }) = event {
@@ -194,7 +222,8 @@ fn main() {
                             cursor_x += 1;
                         }
                     }
-                    render_file_data(&file_data, cursor_x, cursor_y);
+                    (window_line_x, window_line_y) = calc_window_lines(&file_data, window_line_x, window_line_y, cursor_x, cursor_y);
+                    render_file_data(&file_data, window_line_x, window_line_y, cursor_x, cursor_y);
                 }
             }
         }
